4db46a124cb51a77e08f8e448b0060ac8018d567
Carlos Moraes <carlosm1412@gmail.com>
2020-01-08 16:25:39 -0300

    Inserindo rodape e animacao no site

--- app/sts/Views/home/home.php
+++ app/sts/Views/home/home.php
@@ -57,7 +57,7 @@ if (!defined('URL')) {
         <div class="container">
             <h2 class="display-4 text-center" style="margin-bottom: 40px;"><?php echo $titulo; ?></h2>
             <div class="card-deck">
-                <div class="card text-center card-um">
+                <div class="card text-center anim_left">
                     <div class="icon-row tamanh-icone">
                         <span class="step size-96 text-danger">
                             <i class="icon <?php echo $icone_um; ?>"></i>
@@ -68,7 +68,7 @@ if (!defined('URL')) {
                         <p class="card-text lead"><?php echo $descricao_um; ?></p>      
                     </div>
                 </div>
-                <div class="card text-center card-dois">
+                <div class="card text-center anim_bottom">
                     <div class="icon-row tamanh-icone">
                         <span class="step size-96 text-danger">
                             <i class="icon <?php echo $icone_dois; ?>"></i>
@@ -79,7 +79,7 @@ if (!defined('URL')) {
                         <p class="card-text lead"><?php echo $descricao_dois; ?></p>
                     </div>
                 </div>
-                <div class="card text-center card-tres">
+                <div class="card text-center anim_right">
                     <div class="icon-row tamanh-icone ">
                         <span class="step size-96 text-danger">
                             <i class="icon <?php echo $icone_tres; ?>"></i>
@@ -97,9 +97,9 @@ if (!defined('URL')) {
     <?php extract($this->Dados['sts_videos'][0]); ?>
     <div class="jumbotron video">
         <div class="container">
-            <h2 class="display-4 text-center video-titulo" style="margin-bottom: 40px;"><?php echo $titulo; ?></h2>
-            <p class="lead text-center video-parag"><?php echo $descricao; ?></p>
-            <div class="row justify-content-md-center video-cont">
+            <h2 class="display-4 text-center anim_left" style="margin-bottom: 40px;"><?php echo $titulo; ?></h2>
+            <p class="lead text-center anim_right"><?php echo $descricao; ?></p>
+            <div class="row justify-content-md-center anim_bottom">
 
                 <div class="col-12 col-md-8">
                     <div class="embed-responsive embed-responsive-16by9">
@@ -121,22 +121,22 @@ if (!defined('URL')) {
                     extract($artigo);
                     ?>
 
-                    <div class="card art-um">
+                    <div class="card anim_bottom">
                         <a href="<?php echo URL . 'artigo/' . $slug; ?>" alt="<?php echo $titulo; ?>"> 
                             <img class="card-img-top" src="<?php echo URL . 'assets/imagens/artigo/' . $id . '/' . $imagem; ?>" alt="<?php echo $titulo ?>">
                         </a>
                         <div class="card-body">
-                            <a href="artigo.html">
+                            <a href="<?php echo URL . 'artigo/' . $slug; ?>" alt="<?php echo $titulo; ?>">
                                 <h5 class="card-title text-center text-danger"><?php echo $titulo; ?></h5>
                             </a>
                             <p class="card-text"><?php echo $descricao; ?></p>
-                            <p class="text-center"><a href="#" class="btn btn-danger">Mais Detalhes</a></p>
+                            <p class="text-center"><a href=""<?php echo URL . 'artigo/' . $slug; ?>" alt="<?php echo $titulo; ?>"" class="btn btn-danger">Mais Detalhes</a></p>
                         </div>
                     </div>
 
-    <?php
-}
-?>
+                    <?php
+                }
+                ?>
 
 
             </div>
